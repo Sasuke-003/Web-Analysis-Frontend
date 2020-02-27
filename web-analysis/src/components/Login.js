ff2f92e325f8f497b4ad99f1a7eea1c23fec36b0
Hariprasad004 <57026806+Hariprasad004@users.noreply.github.com>
2020-02-27 22:33:43 +0530

    login

--- web-analysis/src/components/Login.js
+++ web-analysis/src/components/Login.js
@@ -3,9 +3,40 @@ import React,{ Component } from "react";
 class Login extends Component{
     render(){
         return(
-            <div>
-                
-            </div>
+            
+                <div class="d-flex justify-content-center h-100">
+		        <div class="card">
+		        	<div class="card-header">
+				    <h3>Sign In</h3>
+			        </div>
+			    <div class="card-body">
+				    <form>
+					    <div class="input-group form-group">
+						
+						<input type="text" class="form-control" placeholder="Username">
+						
+					</div>
+					<div class="input-group form-group">
+						
+						<input type="password" class="form-control" placeholder="Password">
+					</div>
+				
+					<div class="form-group">
+						<input type="submit" value="Login" class="btn float-right login_btn">
+					</div>
+				</form>
+			</div>
+			<div class="card-footer">
+				<div class="d-flex justify-content-center links">
+					Don't have an account?<a href="#">Sign Up</a>
+				</div>
+				<div class="d-flex justify-content-center">
+					<a href="#">Forgot your password?</a>
+				</div>
+			</div>
+		</div>
+	</div>
+            
         )
     }
 }
