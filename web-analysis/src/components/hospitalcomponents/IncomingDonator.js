f43fcaf40c148c2800d3de10bdc361bd5d3affa3
Hariprasad004 <57026806+Hariprasad004@users.noreply.github.com>
2020-02-28 04:20:47 +0530

    hospital

--- web-analysis/src/components/hospitalcomponents/IncomingDonator.js
+++ web-analysis/src/components/hospitalcomponents/IncomingDonator.js
@@ -3,7 +3,31 @@ import React,{ Component } from "react";
 class IncomingDonator extends Component {
     render(){
         return(
-            
+            <div className="container">
+                <h2>Modal Example</h2>
+                <button type="button" className="btn btn-primary" data-toggle="modal" data-target="#myModal">
+                    Open modal
+                </button>
+                <div className="modal" id="myModal">
+    <div className="modal-dialog">
+      <div className="modal-content">
+        <div className="modal-header">
+          <h4 className="modal-title" bg-black>Request for blood !..</h4>
+          <button type="button" className="close" data-dismiss="modal">&times;</button>
+        </div>
+        <div className="modal-body">
+          Hospital need blood,Emergency.
+        </div>
+        <div className="modal-footer">
+          <button type="button" className="btn btn-danger" data-dismiss="modal">Accept</button>
+          <button type="button" className="btn btn-danger" data-dismiss="modal">Reject</button>
+        </div>
+        
+      </div>
+    </div>
+  </div>
+  
+</div>
         )
     }
 }
