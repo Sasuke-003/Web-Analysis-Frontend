65d65b5e0efe08bc53e97e71acefd4b0354cb45e
Hariprasad004 <57026806+Hariprasad004@users.noreply.github.com>
2020-02-28 02:28:05 +0530

    kom

--- web-analysis/src/components/hospitalcomponents/HospitalNavbar.js
+++ web-analysis/src/components/hospitalcomponents/HospitalNavbar.js
@@ -3,7 +3,12 @@ import React,{ Component } from "react";
 class hospitalNavbar extends Component {
     render(){
         return(
-            
+            <div class="topnav">
+  <a className="active" href="#donate">Donate Request</a>
+  <a href="#acceopt">Accepted Request</a>
+  <a href="#income">Incoming Request</a>
+  <a href="#history">Donation History</a>
+</div>
         )
     }
 }
