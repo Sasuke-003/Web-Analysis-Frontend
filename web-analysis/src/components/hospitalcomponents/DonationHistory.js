fd8221c4cef887a8e600259a2651cd34682105a8
Hariprasad004 <57026806+Hariprasad004@users.noreply.github.com>
2020-02-28 05:42:49 +0530

    dsfdf

--- web-analysis/src/components/hospitalcomponents/DonationHistory.js
+++ web-analysis/src/components/hospitalcomponents/DonationHistory.js
@@ -3,7 +3,77 @@ import React,{ Component } from "react";
 class DonationRequest extends Component {
     render(){
         return(
-            
+            <div className="container">
+  <h2 style="background-color: black;color: brown;">User Donation</h2>            
+  <table className="table table-bordered" style="border-collapse:separate;counter-increment: inherit;background-color: khaki;right: gap 50px;;">
+    <thead>
+      <tr>
+        <th>Name</th>
+        <th>E-mail</th>
+        <th>Contact</th>
+        <th>Blood group</th>
+        <th>Date</th>
+      </tr>
+    </thead>
+    <tbody style="row-gap: 50px">
+      <tr >
+        <td></td>
+        <td></td>
+        <td> </td><!--email-->
+      </tr>
+      <tr>
+        <td> </td>
+        <td> </td>
+        <td> </td>
+      </tr>
+      <tr>
+        <td> </td>
+        <td> </td>
+        <td> </td>
+      </tr>
+    </tbody>
+  </table>
+  
+  
+  </div>
+  
+
+
+
+<div className="container">
+  <h2 style="background-color: black;color: brown;">User Donation</h2>            
+  <table className="table table-bordered" style="border-collapse:separate;counter-increment: inherit;background-color: khaki;right: gap 50px;;">
+    <thead>
+      <tr>
+        <th>Name</th>
+        <th>E-mail</th>
+        <th>Contact</th>
+        <th>Blood group</th>
+        <th>Date</th>
+      </tr>
+    </thead>
+    <tbody style="row-gap: 50px">
+      <tr >
+        <td></td>
+        <td></td>
+        <td> </td><!--email-->
+      </tr>
+      <tr>
+        <td> </td>
+        <td> </td>
+        <td> </td>
+      </tr>
+      <tr>
+        <td> </td>
+        <td> </td>
+        <td> </td>
+      </tr>
+    </tbody>
+  </table>
+  
+  
+  </div>
+  
         )
     }
 }
--- web-analysis/src/components/usercomponents/IncomingRequest.js
+++ web-analysis/src/components/usercomponents/IncomingRequest.js
@@ -3,7 +3,31 @@ import React,{ Component } from "react";
 class IncomingRequest extends Component {
     render(){
         return(
-            
+            <div className="container">
+                <h2>Modal Example</h2>
+                <button type="button" className="btn btn-primary" data-toggle="modal" data-target="#myModal">
+                    Open modal
+                </button>
+                <div className="modal" id="myModal">
+    <div className="modal-dialog">
+      <div className="modal-content">
+        <div className="modal-header">
+          <h4 className="modal-title" bg-black>Request to Donate !..</h4>
+          <button type="button" className="close" data-dismiss="modal">&times;</button>
+        </div>
+        <div className="modal-body">
+          I like to donate the blood.
+        </div>
+        <div className="modal-footer">
+          <button type="button" className="btn btn-danger" data-dismiss="modal">Accept</button>
+          <button type="button" className="btn btn-danger" data-dismiss="modal">Reject</button>
+        </div>
+        
+      </div>
+    </div>
+  </div>
+  
+</div>
         )
     }
 }
